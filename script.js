33be4faf44b26f8d5049997845bc42f01db3b8ad
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-15 23:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+
+  const lowerStr = s1.toLowerCase();
+  const lowerSubStr = s2.toLowerCase();
+  // Find the index of the first occurrence of the lowercase subStr in the lowercase str
+  const index = lowerStr.indexOf(lowerSubStr);
+  // If the index is -1, it means the subStr is not found in the str
+  if (index === -1) {
+    return -1;
+  }
+  // Return the original index of the first occurrence of subStr in str
+  return index;
 }
 
 // Please do not change the code below
